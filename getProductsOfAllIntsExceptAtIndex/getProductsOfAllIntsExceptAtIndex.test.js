6f0031e29f2fd484796135dfcacbf6c110e92af4
Enoch Kim <enochkim91@gmail.com>
2017-05-22 20:21:55 -0700

    Completes isEqual function and tests for comparing two arrays

--- getProductsOfAllIntsExceptAtIndex/getProductsOfAllIntsExceptAtIndex.test.js
+++ getProductsOfAllIntsExceptAtIndex/getProductsOfAllIntsExceptAtIndex.test.js
@@ -5,17 +5,32 @@ const getProductsOfAllIntsExceptAtIndex = require('./getProductsOfAllIntsExceptA
 // Build my own equality function for practice - use jest's '.toBeTruthy()'
 
 let isEqual = (array1, array2) => {
-  //first check if the two arrays are the same length
-    //return false if they arent the same
-  // loop through each index and see if they are true
-    // if they are return true
-    // return false
+  let isEqual = true
+
+  if (array1.length !== array2.length) {
+    return false;
+  }
+  // array1.length !== array2.length ? false : true
+
+  array1.forEach((i, index) => {
+    if ( i !== array2[index]){
+      isEqual = false;
+    }
+  })
+
+  return isEqual;
 }
 
+let array1 = [1, 7, 3, 4];
+let array2 = [1, 7, 3, 5];
+console.log(isEqual(array1, array2))
 
-test('testing toEqual', () => {
+test('testing isEqual function', () => {
   let array1 = [1, 7, 3, 4];
   let array2 = [1, 7, 3, 4];
-  expect(array1).toEqual(array2);
+  let array3 = [1, 7, 3, 10];
 
+  expect(isEqual(array1, array2)).toBeTruthy()
+  expect(isEqual(array1, array3)).toBeFalsy()
 })
+
