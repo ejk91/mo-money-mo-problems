173b38e78fb07211af1632da41fe23fb57d4cebb
Enoch Kim <enochkim91@gmail.com>
2017-05-22 19:26:56 -0700

    Set up original problem info

---
+++ getProductsOfAllIntsExceptAtIndex/getProductsOfAllIntsExceptAtIndex.js
@@ -0,0 +1,14 @@
+/*
+Write a function getProductsOfAllIntsExceptAtIndex() that takes an array of integers 
+and returns an array of the products.
+
+For Example:
+
+[1, 7, 3, 4] => [84, 12, 28, 21]
+
+This is done by calculating:
+[7 * 3 * 4,  1 * 3 * 4,  1 * 7 * 4,  1 * 7 * 3]
+
+Do not use division.
+*/
+
