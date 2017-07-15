6bb13e34e21691b315d8c224dfb2ec8dc8c6fa8c
Enoch Kim <enochkim91@gmail.com>
2017-07-15 16:36:29 -0700

    Initial setup for changeSum algo

---
+++ changeSum/changeSum.js
@@ -0,0 +1,19 @@
+/*
+Write a function that, given:
+
+1. an amount of money
+2. an array of coin denominations
+
+computes the number of ways to make the amount of money with
+coins of the available denominations.
+
+Example: for amount=(4¢) and denominations= [1,2,3] (1¢, 2¢ and 3¢),
+your program would output 4—the number of ways to make 
+4¢ with those denominations:
+
+1¢, 1¢, 1¢, 1¢
+1¢, 1¢, 2¢
+1¢, 3¢
+2¢, 2¢
+
+*/
